b4c50799d1375a096b6f5b46d8ce61449cecfe67
dcamy <diegocamyy@gmail.com>
2021-10-17 18:40:04 -0300

    Show spinner component when fetching search history from database

--- frontend/src/pages/History/History.tsx
+++ frontend/src/pages/History/History.tsx
@@ -1,18 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Spinner from "../../components/Spinner/Spinner";
 import { SearchHistoryObject } from "../../interfaces/searchHistoryInterface";
 import "./History.css";
 
 export const History = () => {
   const [searchHistory, setSearchHistory] = useState<SearchHistoryObject[]>([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     async function fetchHistory() {
+      setLoading(true);
       try {
         const { data } = await axios.get<SearchHistoryObject[]>("/api/search");
         setSearchHistory(data);
       } catch (error) {
         console.log(error);
       }
+      setLoading(false);
     }
 
     fetchHistory();
@@ -20,24 +25,28 @@ export const History = () => {
   return (
     <div className="history-container">
       <h2>Historial de búsquedas</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>IP</th>
-            <th>Artista</th>
-            <th>Fecha</th>
-          </tr>
-        </thead>
-        <tbody>
-          {searchHistory.map((e) => (
-            <tr key={e.id}>
-              <td>{e.ip}</td>
-              <td>{e.artist}</td>
-              <td>{new Date(e.created_at).toLocaleString()}</td>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>IP</th>
+              <th>Artista</th>
+              <th>Fecha</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {searchHistory.map((e) => (
+              <tr key={e.id}>
+                <td>{e.ip}</td>
+                <td>{e.artist}</td>
+                <td>{new Date(e.created_at).toLocaleString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
